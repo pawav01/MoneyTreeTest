2b637dacdc90b0d668371418f91c9950aa9e9be0
DeoraAakriti <111840471+DeoraAakriti@users.noreply.github.com>
2022-11-23 11:10:08 -0500

    Adding Login and Signup pages

--- Web/moneytreeweb/src/components/Login.js
+++ Web/moneytreeweb/src/components/Login.js
@@ -1,6 +1,48 @@
-import React from "react";
-const Login = () => {
-  return <div></div>;
-};
+import '../styles/Login.css';
+import profile from "../images/a.png";
+import email from "../images/email.jpg";
+import pass from "../images/pass.png";
+import {Link} from "react-router-dom";
+
+function Login() {
+  return (
+    <div className="main">
+     <div className="sub-main">
+       <div>
+         <div className="imgs">
+           <div className="container-image">
+             <img src={profile} alt="profile" className="profile"/>
+
+           </div>
+
+
+         </div>
+         <div>
+           <h1>Login Page</h1>
+           <div>
+             <img src={email} alt="email" className="email"/>
+             <input type="text" placeholder="user name" className="name"/>
+           </div>
+           <div className="second-input">
+             <img src={pass} alt="pass" className="email"/>
+             <input type="password" placeholder="password" className="name"/>
+           </div>
+          <div className="login-button">
+          <button>Login</button>
+          </div>
+           
+            <p className="link">
+              <a href="https://Google.com">Forgot password ?</a> Or <Link to ="/signup" >Sign Up </Link>
+            </p>
+           
+ 
+         </div>
+       </div>
+       
+
+     </div>
+    </div>
+  );
+}
 
 export default Login;

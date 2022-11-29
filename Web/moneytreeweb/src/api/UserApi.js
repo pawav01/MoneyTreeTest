b55c5851298b3bb20271f79e52fe5501f3907218
Ved Pawar <pawav01@pfw.edu>
2022-11-28 20:33:51 -0500

    create user api call

--- Web/moneytreeweb/src/api/UserApi.js
+++ Web/moneytreeweb/src/api/UserApi.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 
-export const getUserData = async() => {
+export const getUserData = async () => {
 
     var data = fetch('http://localhost:4000/users/3')
         .then(response => response.json())
@@ -8,6 +8,24 @@ export const getUserData = async() => {
             return response[0];
         })
         .catch(err => console.error(err));
-    
+
     return data;
-    }
+}
+
+export const createUser = async (newUser) => {
+    console.log(" ++++ " + JSON.stringify(newUser));
+    var data = fetch('http://localhost:4000/users', { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(newUser)
+        })
+        .then(response => response.json())
+        .then(response => {
+            return response[0];
+        })
+        .catch(err => console.error(err));
+
+    return data;
+}
